8cb99ecae30a83d4622a7ee020d17e0e15b87108
Luis Enrique Perez Galindo <luis_perez992@hotmail.com>
2019-03-04 17:06:28 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            caca popo chis React
           </a>
         </header>
       </div>
